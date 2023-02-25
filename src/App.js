b0af5b184c71d7e908c12304b6bae5520ebbe03d
amadich <jeridi.moha@gmail.com>
2023-02-25 20:25:26 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import MasterW from './components/MasterW';
+import './css/bootstrap.min.css';
+import './js/bootstrap.bundle.min.js';
+function App() {
+  return (
+    <>
+        <Header />
+        <MasterW />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MasterW.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import '../css/MasterW.css';
+export default class MasterW extends Component {
+   render() {
+      return(
+         <article>
+            <nav>
+               <h1>Geo Master World</h1>
+               <h4>Freeom World Every Time</h4>
+               <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime dolor exercitationem explicabo nulla minus voluptatum vel fugit rerum, quod illum, dignissimos sit fugiat error debitis libero neque ratione perspiciatis quia?
+               </p>
+
+               <button>Learn More</button>
+            </nav>
+         </article>
+      )
+   }
+}
